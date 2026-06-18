ffb1d54df0e94848c9a6558ecaa32e180052c011
edrianmendoza-yon <edrian.mendoza@awsys-i.com>
2026-06-18 14:59:41 +0800

    fixed output commit

--- api.js
+++ api.js
@@ -12,7 +12,7 @@ let mockStudentDb = [
     new Student(Date.now(), "Lightning", "McQueen", "lightning.mcqueen@example.com"),
     new Student(Date.now() + 1, "Woody", "Pride", "woody.pride@example.com"),
 ];
-console.log(`Initial DB State: ${JSON.stringify(mockStudentDb)}`);
+console.log(`Initial DB State: ${JSON.stringify(mockStudentDb, null, 2)}`);
 
 // This function simulates the GET request to a backend server.
 let getStudentsAPI = () => {
@@ -33,5 +33,5 @@ let addStudentAPI = (newStudentData) => {
 }
 
 addStudentAPI({ firstName: "Light", lastName: "Yagami", email: "light.yagami@example.com" });
-console.log(`Returned Saved Object: ${JSON.stringify(mockStudentDb[2])}`);
-console.log(`Updated DB State: ${JSON.stringify(getStudentsAPI())}`);
+console.log(`Returned Saved Object: ${JSON.stringify(mockStudentDb[2], null, 2)}`);
+console.log(`Updated DB State: ${JSON.stringify(getStudentsAPI(), null, 2)}`);
